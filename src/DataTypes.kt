c4882c05d5315c1ee32777ea7ca95d7274d307da
Sagar Dhadke <peaceunique2003@gmail.com>
2024-03-19 15:53:44 +0530

    DataTypes in Kotlin

---
+++ src/DataTypes.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    //DataTypes in Kotlin
+
+    //int, float, double, String, CHar, boolean, Long, Short, byte.
+
+    /*
+     val manual : Int = 445
+     */
+
+    val number = 4
+    val doubleNumber = 4.99
+    val floatNumber = 3.14f
+    val latter = 'A'
+    val boolean = true
+    val text = "Hey My Name is Sagar"
+
+}
