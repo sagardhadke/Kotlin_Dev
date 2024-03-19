5e422d821a0ed87b86297e6b8272689dd358aca1
Sagar Dhadke <peaceunique2003@gmail.com>
2024-03-19 17:05:36 +0530

    Condition in Kotlin

---
+++ src/Condtions.kt
@@ -0,0 +1,40 @@
+fun main() {
+    ageVerify(18)
+
+    //check number
+   val checkSign = checkNumberSign(20)
+    println(checkSign)
+}
+
+//age verification
+fun ageVerify(age:Int){
+    if (age < 18){
+        println("Access Denied!")
+    }
+
+    //clean code
+//    if (age > 18 || age == 18) println("Access Approved!")
+    if (age >= 18) println("Access Approved!")
+
+}
+
+
+fun checkNumberSign(number : Int) : String{
+    return if (number > 0){
+        "Positive"
+    }else if (number < 0){
+        "Negative"
+    }else{
+        "Zero"
+    }
+}
+
+
+
+
+
+
+
+
+
+
