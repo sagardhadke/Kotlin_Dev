c104809d3965f82c69203894e01a706667a66036
Sagar Dhadke <peaceunique2003@gmail.com>
2024-03-19 15:38:05 +0530

    Variables in Kotlin

---
+++ src/Variable.kt
@@ -0,0 +1,13 @@
+fun main() {
+    //variables in kotlin
+
+    //var -> you can change the value
+    var name = "Java"
+    name = "Kotlin"
+    println(name) // Printing latest value
+
+    //val -> you can't change the value
+    val age = 19
+//    age = 20  // Showing error because val can't reassign
+    println(age)
+}
