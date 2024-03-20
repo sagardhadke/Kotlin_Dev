865b308a1d3b5007b3383a278a5348cc5a060bf5
Sagar Dhadke <peaceunique2003@gmail.com>
2024-03-20 10:32:24 +0530

    Print Each String char using while loop

--- src/Loops.kt
+++ src/Loops.kt
@@ -8,7 +8,7 @@ fun main() {
          num++
     }
 
-    //while loop with function
+//    while loop with function
     var a = 0
     while (condition(a=a)){
         println(a)
@@ -21,8 +21,21 @@ fun main() {
         number += 10
     }while (number<=100)
 
+    displayContent("Sagar Dhadke")
+
 }
 
+fun displayContent(text: String){
+//    println(text[0])    //get first charector
+
+   //print char each and every line
+    var i = 0
+    while (i < text.length){
+        println(text[i])
+        i++
+    }
+
+}
 
 fun condition(a : Int) : Boolean {
     return  a<=5
