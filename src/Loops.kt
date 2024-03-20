f1a402cfc6e9a9f6f3d418df894f7459a4bfb8dc
Sagar Dhadke <peaceunique2003@gmail.com>
2024-03-20 10:47:13 +0530

    For loop in Kotlin

--- src/Loops.kt
+++ src/Loops.kt
@@ -2,41 +2,54 @@ fun main() {
 
     //Loops In Kotlin
 
-    var num = 0
-    while (num < 10){
+    //for loop
+//    val elements = arrayOf("Sagar", "Mango", "Audi", "Kiwi", "Jay")
+//    for(i in elements){
+//        println(i)
+//    }
+
+    //print number using fo loop
+    for (num in 1..5) {
         println(num)
-         num++
-    }
-
-//    while loop with function
-    var a = 0
-    while (condition(a=a)){
-        println(a)
-        a++
-    }
-
-    var number = 0
-    do {
-        println(number)
-        number += 10
-    }while (number<=100)
-
-    displayContent("Sagar Dhadke")
-
-}
-
-fun displayContent(text: String){
-//    println(text[0])    //get first charector
-
-   //print char each and every line
-    var i = 0
-    while (i < text.length){
-        println(text[i])
-        i++
     }
 
 }
 
-fun condition(a : Int) : Boolean {
-    return  a<=5
-}
+//    var num = 0
+//    while (num < 10){
+//        println(num)
+//         num++
+//    }
+//
+////    while loop with function
+//    var a = 0
+//    while (condition(a=a)){
+//        println(a)
+//        a++
+//    }
+//
+//    var number = 0
+//    do {
+//        println(number)
+//        number += 10
+//    }while (number<=100)
+//
+//    displayContent("Sagar Dhadke")
+//
+//}
+//
+//fun displayContent(text: String){
+////    println(text[0])    //get first charector
+//
+//   //print char each and every line
+//    var i = 0
+//    while (i < text.length){
+//        println(text[i])
+//        i++
+//    }
+//
+//}
+//
+//fun condition(a : Int) : Boolean {
+//    return  a<=5
+//}
---
+++ src/Loops/For.kt
@@ -0,0 +1,23 @@
+package Loops
+
+fun main() {
+
+    //for loop
+
+    //printing values 1 to 5
+    for (a in 1..5){
+        println(a)
+    }
+
+    //printing values 10 to 1 reverse form
+    for (b in 10 downTo 1){
+        println(b)
+    }
+
+    //printing table like 2
+    for (table in 2..20 step 2){
+        println(table)
+    }
+
+
+}
