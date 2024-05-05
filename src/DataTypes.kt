6b53e7395e4765b8e92bf14cbe7a7b8c3a2e6698
Sagar Dhadke <peaceunique2003@gmail.com>
2024-05-05 14:35:24 +0530

    All Datatypes in Kotlin

--- src/DataTypes.kt
+++ src/DataTypes.kt
@@ -1,3 +1,6 @@
+import kotlin.math.min
+import kotlin.math.truncate
+
 fun main() {
 
     //DataTypes in Kotlin
@@ -14,5 +17,16 @@ fun main() {
     val latter = 'A'
     val boolean = true
     val text = "Hey My Name is Sagar"
+
+    //All Types of DataTypes in Kotlin
+
+    val a : Int = 35512
+    val b : Float = 3.4f
+    val c : Double = 32.32
+    val d : String = "Sagar"
+    val e : Short = 32656
+    val f : Long = 4863147863258965666L
+    val g : Boolean = true // true and false
+    val h : Char = 'A' // 'A-Z' and 'a-z' @
 
 }
