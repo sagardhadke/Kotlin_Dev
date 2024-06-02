87c3a0d970466b7ec6b457cfe1777a1d491d46e8
Sagar Dhadke <peaceunique2003@gmail.com>
2024-06-02 20:22:35 +0530

    Enum

---
+++ src/Oops/Other/Enum.kt
@@ -0,0 +1,23 @@
+package Oops.Other
+
+import Oops.Other.Enum
+
+enum class Enum() {
+
+    SUN,
+    MON,
+    TUE,
+    WED,
+    THR,
+    FRI,
+    SAT;
+
+}
+
+fun main() {
+
+
+    var myObj = Enum.MON
+    println(myObj)
+
+}
